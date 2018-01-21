d530e043e4eda9d60a5ddd2444b40b13659dfc3a
Sebastian <sebastian.ptz@outlook.com>
2018-01-21 23:30:12 +0100

    Update Item.java
    comments and declaration only

--- Item.java
+++ Item.java
@@ -1,31 +1,35 @@
 /**
  * Write a description of class Item here.
  * 
- * @author Thu Ky Vu Hoang, Sebastian Pütz
+ * @author Thu Ky Vu Hoang, Sebastian PÃ¼tz
  * @version 2018.01.21
  */
 public class Item
 {
     // instance variables - replace the example below with your own
-    private String name;
     private int weight;
-    private String description;
-    private boolean pickable;
-    private boolean eatable;
+    private String name, description;
+    private boolean pickable, eatable;
 
     /**
-     * Constructor for objects of class Item
+     * Default Constructor for objects of class Item
      */
     public Item(String name, String description, int weight)
     {
         this(name, description, weight, false, true);
     }
 
+    /**
+     * Constructor for an object of class Item that can be used with the command eat
+     */
     public Item(String name, String description, int weight, boolean eatable)
     {
         this(name, description, weight, eatable, true);
     }
 
+    /**
+     * Constructor for an object of class Item that can be used with the command eat, take
+     */
     public Item(String name, String description, int weight, boolean eatable, boolean pickable)
     {
         this.name = name;
@@ -35,9 +39,13 @@ public class Item
         this.pickable = pickable;
     }
     
+    /**
+     *return the details of an item
+     */
     public String getDetails() {
         return name + "(" + weight + "): " + description;
     }
+    
     /**
      * @return name of an item.
      */
@@ -59,6 +67,13 @@ public class Item
         return weight;
     }
     
+    /**
+     * @return that an item eaten by the actor
+     */
     public boolean isEatable() { return eatable; }
+    
+    /**
+     * @return that an item can be taken by the actor
+     */
     public boolean isPickable() { return pickable; }
 }
